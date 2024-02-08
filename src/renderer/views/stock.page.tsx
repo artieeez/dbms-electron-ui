0306fc47b85e95ea58373eee3b7382fb41849b93
Artur <artur.webber@networkme.io>
2024-02-08 13:33:49 -0300

    feat: aaa

--- src/renderer/views/stock.page.tsx
+++ src/renderer/views/stock.page.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Collapse, Container, Stack, TextField, Typography } from "@mui/material"
+import { Box, Button, CardHeader, Collapse, Container, Stack, TextField, Typography } from "@mui/material"
 import { DataGrid } from "@mui/x-data-grid"
 import { useEffect, useState } from "react";
 import { Stock } from "../infra/dbms.model";
 import { StockListService } from "../service/stock-list.service";
 import { StockService } from "../service/stock.service";
+import { SearchRounded } from "@mui/icons-material";
 
 const dbms = window.dbms
 
@@ -29,56 +30,64 @@ export const StockPage = () => {
 
   return (
     <Container sx={{ py: 4 }}>
-      <Typography variant="h5">Stock Page</Typography>
-      {/* Add stock */}
-
-      <Button
-        variant="contained"
-        onClick={()=>setOpenAdd(!openAdd)}
-        sx={{mt: 2}}
-      >
-        Add Stock
-      </Button>
-      <Collapse in={openAdd} sx={{py: 2}}>
-      <Box display="flex" flexDirection="row" gap={2}>
-        <TextField
-          label="Stock ID"
-          value={stockId}
-          onChange={(e) => setStockId(e.target.value)}
-          sx={{ mb: 2 }}
-        />
-        <TextField
-          label="Company ID"
-          value={companyId}
-          onChange={(e) => setCompanyId(e.target.value)}
-          sx={{ mb: 2 }}
-        />
-        <TextField
-          label="Min Date"
-          value={minDate}
-          onChange={(e) => setMinDate(e.target.value)}
-          sx={{ mb: 2 }}
-        />
-        <TextField
-          label="Max Date"
-          value={maxDate}
-          onChange={(e) => setMaxDate(e.target.value)}
-          sx={{ mb: 2 }}
-        />
-        
-      </Box>
-        <Button
-          variant="contained"
-          onClick={addStock}
+      <CardHeader
+        title="Stock Page"
+        subheader="Welcome to the Stock Page"
+        action={
+          <Button
+            variant="contained"
+            onClick={() => setOpenAdd(!openAdd)}
+            sx={{ mt: 2 }}
+          >
+            Add Stock
+          </Button>
+        }
+        // remove left padding
+        sx={{ pl: 0 }}
+      />
+      <Collapse in={openAdd} sx={{ py: 2 }}>
+        <Box display="flex" flexDirection="row" gap={2}>
+          <TextField
+            label="Stock ID"
+            value={stockId}
+            onChange={(e) => setStockId(e.target.value)}
+          />
+          <TextField
+            label="Company ID"
+            value={companyId}
+            onChange={(e) => setCompanyId(e.target.value)}
+          />
+          <TextField
+            label="Min Date"
+            value={minDate}
+            onChange={(e) => setMinDate(e.target.value)}
+          />
+          <TextField
+            label="Max Date"
+            value={maxDate}
+            onChange={(e) => setMaxDate(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            onClick={addStock}
+            sx={{ flex: 1 }}
           >
             Confirm
-        </Button>
+          </Button>
+        </Box>
       </Collapse>
       <TextField
-        label="Search"
+        fullWidth
         value={search}
         onChange={(e) => StockListService.useStore.setState({ search: e.target.value })}
         sx={{ mb: 2 }}
+        InputProps={{
+          startAdornment: (
+            <Box sx={{ mr: 1 }}>
+              <SearchRounded />
+            </Box>
+          ),
+        }}
       />
       <DataGrid
         rows={query?.data || []}

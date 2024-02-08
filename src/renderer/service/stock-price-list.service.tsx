2c63b44e32c6a3eb33e0752101ced770a03d50ec
Artur <artur.webber@networkme.io>
2024-02-08 14:00:02 -0300

    a

--- src/renderer/service/stock-price-list.service.tsx
+++ src/renderer/service/stock-price-list.service.tsx
@@ -6,15 +6,15 @@ interface Store {
   page: number;
   pageSize: number;
   minDate: string;
-    maxDate: string;
+  maxDate: string;
 }
 
 const useStore = create<Store>((set) => ({
-    stockId: '',
+  stockId: '',
   page: 0,
   pageSize: 10,
-    minDate: '',
-    maxDate: '',
+  minDate: '',
+  maxDate: '',
 }))
 
 const useStockPriceQuery = () => {
@@ -22,13 +22,11 @@ const useStockPriceQuery = () => {
   const stockId = useStore(e => e.stockId)
   const page = useStore(e => e.page)
   const pageSize = useStore(e => e.pageSize)
-  const minDate = useStore(e => e.minDate)
-    const maxDate = useStore(e => e.maxDate)
 
   return useQuery({
     queryKey: ['stock-price', stockId, pageSize, page],
     queryFn: async () => {
-      const res = window.dbms?.linearSearch?.getStockPriceList(stockId, minDate, maxDate, "name_asc")
+      const res = window.dbms?.indexController?.getStockPriceList(stockId, page, pageSize)
       return res
     },
   })
